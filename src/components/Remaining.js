a4409b24632e93661173013fe229775cef518a88
Kara LeSueur <klesueur@kornerstonecredit.com>
2022-02-08 21:25:25 -0800

    just beginning components

---
+++ src/components/Remaining.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Remaining = () => {
+    return (
+        <div className='alert alert-success'>
+            <span> Remaining: $800 </span>
+        </div>
+    )
+};
+
+export default Remaining;
